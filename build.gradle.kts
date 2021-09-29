93b472db6c131ffd0429dd00c12b1af4ac0f5fa4
Bence <pirosgyb@gmail.com>
2021-09-29 11:29:58 +0200

    Fixed missing import

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,5 +35,6 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("org.postgresql:postgresql:$postgres_version")
     implementation("com.zaxxer:HikariCP:$hikaricp_version")
+    implementation("io.ktor:ktor-client-okhttp:$ktor_version")
     implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
 }
